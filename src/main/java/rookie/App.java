49337b4849b91c9369072c4bfb8750507ba3040a
AleBaltieri <19285@studenti.marconiverona.edu.it>
2023-06-12 17:31:53 +0200

    Creazione controller dottore

---
+++ src/main/java/rookie/App.java
@@ -0,0 +1,12 @@
+package rookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication springApplication=new SpringApplication(App.class);
+        springApplication.run(args);
+    }
+}
---
+++ src/main/java/rookie/dto/DoctorDTO.java
@@ -0,0 +1,78 @@
+package rookie.dto;
+
+public class DoctorDTO {
+    private Long id;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String address;
+    private String email;
+    private String avatar;
+    private String profession;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+
+}
