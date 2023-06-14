eb7fb08a784e9c2cf8d00f718ad51face39ee5b1
Filippo <filippo.soff05@gmail.com>
2023-06-14 13:08:13 +0200

    Creazione Filter

--- src/main/java/org/example/service/DoctorService.java
+++ src/main/java/org/example/service/DoctorService.java
@@ -2,10 +2,16 @@ package org.example.service;
 
 import org.example.domain.Doctor;
 import org.example.dto.DoctorDTO;
+import org.example.dto.DoctorFilterDTO;
 import org.example.mapper.DoctorMapper;
 import org.example.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DoctorService {
@@ -17,5 +23,12 @@ public class DoctorService {
         Doctor doctor = mapper.toEntity (doctorDTO);
         doctorRepository.save (doctor);
         return mapper.toDTO (doctor);
+    }
+
+    public List<DoctorDTO> getListDoctor(Pageable pageable, DoctorFilterDTO filter) {
+        List<Doctor> doctors = doctorRepository.getDoctors (pageable, filter.getName(), filter.getSurname(), filter.getProfession());
+        return doctors.stream()
+                .map(mapper::toDTO)
+                .toList();
     }
 }
