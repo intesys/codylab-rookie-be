9041d678e78375e76f97c24b83402a0696ef0045
Richmond Zoogah <richiezoogah@yahoo.com>
2023-06-14 18:12:53 +0200

    Giorno 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,5 @@
 package org.example;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/org/example/Mapper.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public interface Mapper <ENTITY, DTO>{
-    ENTITY toEntity(DTO doctorDto);
-
-    DTO toDo(ENTITY entity);
-}
