ce5eef345573fb18da5e2bea6f50f65af283510e
Francesco <francymannarino05@gmail.com>
2023-06-14 12:30:13 +0200

    aggiunzione ordinamento con filtro (dottori)

--- src/main/java/org/example/api/DoctorApi.java
+++ src/main/java/org/example/api/DoctorApi.java
@@ -1,18 +1,37 @@
 package org.example.api;
 
 import org.example.dto.DoctorDTO;
+import org.example.dto.DoctorFilterDTO;
 import org.example.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DoctorApi {
     @Autowired
     private DoctorService doctorService;
+
     @PostMapping ("/api/doctor")
-    ResponseEntity<Object> createDoctor (@RequestBody DoctorDTO doctorDTO) {
-        DoctorDTO doctor = doctorService.createDoctor(doctorDTO);
-        return ResponseEntity.ok(doctor);
+    ResponseEntity<DoctorDTO> createDoctor (@RequestBody DoctorDTO doctorDTO) {
+        doctorDTO = doctorService.createDoctor(doctorDTO);
+        return ResponseEntity.ok(doctorDTO);
     }
+
+    @PostMapping ("/api/doctor/filter")
+    ResponseEntity<List<DoctorDTO>> getListDoctor (@RequestParam("page") Integer pageIndex, @RequestParam("size") Integer size, @RequestParam ("sort") String sort, @RequestBody DoctorFilterDTO filter) {
+        Pageable pageable = pageable (pageIndex, size, sort);
+
+        List<DoctorDTO> doctorDTOs = doctorService.getListDoctor (pageable, filter);
+        return ResponseEntity.ok(doctorDTOs);
+    }
+
+    private Pageable pageable(Integer pageIndex, Integer size, String sort) {
+        return PageRequest.of(pageIndex, size, Sort.unsorted());
+    }
+
+
 }
--- src/main/java/org/example/service/DoctorService.java
+++ src/main/java/org/example/service/DoctorService.java
@@ -2,10 +2,17 @@ package org.example.service;
 
 import org.example.domain.Doctor;
 import org.example.dto.DoctorDTO;
+import org.example.dto.DoctorFilterDTO;
 import org.example.mapper.DoctorMapper;
 import org.example.repository.DoctorRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DoctorService {
@@ -18,4 +25,13 @@ public class DoctorService {
         doctorRepository.save (doctor);
         return mapper.toDTO (doctor);
     }
+
+    public List<DoctorDTO> getListDoctor(Pageable pageable, DoctorFilterDTO filter) {
+        List<Doctor> doctors = doctorRepository.getDoctors (pageable, filter.getName(), filter.getSurname(), filter.getProfession());
+        return doctors.stream()
+                .map(mapper::toDTO)
+                .toList();
+    }
 }
+
+//:: mi permettono di recuperare metodo (clausure penso)
