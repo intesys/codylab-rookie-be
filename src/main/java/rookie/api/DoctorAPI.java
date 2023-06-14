3970ed39e97a2a9e692d438381714e1b69e51abc
aleba <19285@studenti.marconiverona.edu.it>
2023-06-14 16:26:19 +0200

    liste dottori

--- src/main/java/rookie/api/DoctorAPI.java
+++ src/main/java/rookie/api/DoctorAPI.java
@@ -1,12 +1,14 @@
 package rookie.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.web.bind.annotation.*;
 import rookie.dto.DoctorDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import rookie.dto.DoctorFIlterDTO;
 import rookie.service.DoctorService;
+
+import java.util.List;
 
 @RestController
 public class DoctorAPI {
@@ -14,7 +16,17 @@ public class DoctorAPI {
     private DoctorService doctorService;
     @PostMapping("/api/doctor")
      ResponseEntity<DoctorDTO> createDoctor(@RequestBody DoctorDTO doctorDTO){
-        DoctorDTO doctor = doctorService.createDoctor(doctorDTO);
-        return ResponseEntity.ok(doctor);
+        doctorDTO = doctorService.createDoctor(doctorDTO);
+        return ResponseEntity.ok(doctorDTO);
+    }
+    @PostMapping("/api/doctor/filter")
+    ResponseEntity<List<DoctorDTO>> getListDoctor(@RequestParam("page") Integer pageIndex, @RequestParam("size") Integer size, @RequestParam("sort") String sort, @RequestBody DoctorFIlterDTO filter){
+        Pageable pageable=pageable(pageIndex,size,sort);
+        List<DoctorDTO> doctorDTOS = doctorService.getListDoctor(pageable, filter);
+        return ResponseEntity.ok(doctorDTOS);
+    }
+
+    private Pageable pageable(Integer pageIndex, Integer size, String sort) {
+        return PageRequest.of(pageIndex,size, Sort.unsorted());
     }
 }
