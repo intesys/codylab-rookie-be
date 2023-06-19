483e5d34fd5d532b9ea37318460500ffffce5678
Filippo <filippo.soff05@gmail.com>
2023-06-19 16:46:18 +0200

    Implementazione Paziente

---
+++ src/main/java/org/example/service/PatientRecordService.java
@@ -0,0 +1,46 @@
+package org.example.service;
+
+import org.example.domain.PatientRecord;
+import org.example.dto.PatientRecordDTO;
+import org.example.exceptions.NotFound;
+import org.example.mapper.PatientRecordMapper;
+import org.example.repository.PatientRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+
+@Service
+public class PatientRecordService {
+    @Autowired
+    private PatientRecordRepository patientRecordRepository;
+    @Autowired
+    private PatientRecordMapper mapper;
+    public PatientRecordDTO createPatientRecord(PatientRecordDTO patientRecordDTO) {
+        PatientRecord patientRecord = save(patientRecordDTO);
+        return mapper.toDTO (patientRecord);
+    }
+
+    private PatientRecord save(PatientRecordDTO patientRecordDTO) {
+        PatientRecord patientRecord = mapper.toEntity (patientRecordDTO);
+        Instant date = patientRecord.getDate();
+        if (date == null)
+            patientRecord.setDate(Instant.now());
+        patientRecordRepository.save (patientRecord);
+        return patientRecord;
+    }
+
+    public PatientRecordDTO getPatientRecord(Long id) throws NotFound {
+        PatientRecord patientRecord = patientRecordRepository.findById (id);
+        return mapper.toDTO(patientRecord);
+    }
+
+    public void updatePatientRecord(PatientRecordDTO patientRecordDTO)  throws NotFound {
+        save(patientRecordDTO);
+    }
+
+    public void deletePatientRecord(Long id)  throws NotFound {
+        patientRecordRepository.remove (id);
+    }
+}
