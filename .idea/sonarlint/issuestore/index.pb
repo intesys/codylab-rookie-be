967618f633685e44f3ad36a500adf5aedf582c83
Carlo Marchiori <carlo.marchiori@intesys.it>
2023-06-19 10:42:53 +0200

    api patientRecord

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+r
+Bsrc/main/java/it/intesys/codylab/rookie/service/DoctorService.java,0\b\0b3b6ee0466faa5b4de0c9945d62a41b6a52faaf
+j
+:src/main/java/it/intesys/codylab/rookie/api/DoctorApi.java,4\7\47a6968e18f61937dbf5630d0d490173a2faf12e
+j
+:src/main/java/it/intesys/codylab/rookie/api/RookieApi.java,c\7\c7b9b240e19e0a61903c968c5f38c87ebcaa8a07
+j
+:src/main/java/it/intesys/codylab/rookie/domain/Doctor.java,b\7\b78a88b63d597d1399557ac0cf3a6af7737753f2
+p
+@src/main/java/it/intesys/codylab/rookie/mapper/RookieMapper.java,9\5\95bf7f7691cb3a50ab7f7164ef39f22d83db6f3d
+n
+>src/main/java/it/intesys/codylab/rookie/dto/BloodGroupDTO.java,f\7\f7eae96e5171658f19617ddd961260b1c3fd5430
--- src/main/java/it/intesys/codylab/rookie/service/PatientService.java
+++ src/main/java/it/intesys/codylab/rookie/service/PatientService.java
@@ -1,10 +1,14 @@
 package it.intesys.codylab.rookie.service;
 
 import it.intesys.codylab.rookie.domain.Patient;
+import it.intesys.codylab.rookie.domain.PatientRecord;
 import it.intesys.codylab.rookie.dto.PatientDTO;
 import it.intesys.codylab.rookie.dto.PatientFilterDTO;
+import it.intesys.codylab.rookie.dto.PatientRecordDTO;
 import it.intesys.codylab.rookie.exceptions.NotFound;
 import it.intesys.codylab.rookie.mapper.PatientMapper;
+import it.intesys.codylab.rookie.mapper.PatientRecordMapper;
+import it.intesys.codylab.rookie.repository.PatientRecordRepository;
 import it.intesys.codylab.rookie.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -17,7 +21,11 @@ public class PatientService {
     @Autowired
     private PatientRepository patientRepository;
     @Autowired
+    private PatientRecordRepository patientRecordRepository;
+    @Autowired
     private PatientMapper mapper;
+    @Autowired
+    private PatientRecordMapper patientRecordMapper;
     public PatientDTO createPatient(PatientDTO patientDTO) {
         Patient patient = save(patientDTO);
         return mapper.toDTO (patient);
@@ -32,13 +40,27 @@ public class PatientService {
     public List<PatientDTO> getListPatient(Pageable pageable, PatientFilterDTO filter) {
         List<Patient> patients = patientRepository.getPatients (pageable, filter.getText(), filter.getId(), filter.getOpd(), filter.getIdp(), filter.getDoctorId());
         return patients.stream()
-            .map(mapper::toDTO)
+            .map(this::toDTO)
             .toList();
+    }
+
+    private PatientDTO toDTO(Patient patient) {
+        PatientDTO patientDTO = mapper.toDTO(patient);
+        patientDTO.setPatientRecords(patientRecordRepository.findLatestRecordByPatient (patient)
+                .stream()
+                .map(patientRecordMapper::toDTO)
+                .toList());
+        return patientDTO;
     }
 
     public PatientDTO getPatient(Long id) throws NotFound {
         Patient patient = patientRepository.findById (id);
-        return mapper.toDTO(patient);
+        PatientDTO patientDTO = mapper.toDTO(patient);
+        patientDTO.setPatientRecords(patientRecordRepository.findByPatient (patient)
+            .stream()
+            .map(patientRecordMapper::toDTO)
+            .toList());
+        return patientDTO;
     }
 
     public void updatePatient(PatientDTO patientDTO)  throws NotFound {
