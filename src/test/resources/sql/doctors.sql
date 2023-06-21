5a72a7415aabd8cf66b026bd7e989ac0cee30a5f
Filippo <filippo.soff05@gmail.com>
2023-06-21 18:20:50 +0200

    Test

---
+++ src/test/resources/sql/doctors.sql
@@ -0,0 +1,10 @@
+INSERT INTO doctor (id, name,surname,phone_number,address,email,avatar,profession)
+VALUES  (3, 'Rafaello','Testsurname','444 7777777',NULL,'rafaello.marchiori@gmail.com',NULL,'Arrotino'),
+ (5, 'Rafaello','Testsurname','444 7777777',NULL,'rafaello.marchiori@gmail.com',NULL,'Calciatore'),
+  (9, 'Rafaello','Testsurname','444 7777777',NULL,'rafaello.marchiori@gmail.com',NULL,'Nullafacente'),
+    (7, 'Rafaello','Testsurname','444 7777777',NULL,'rafaello.marchiori@gmail.com',NULL,'Programmatore'),
+     (1, 'Rafaello','Testsurname','444 7777777',NULL,'rafaello.marchiori@gmail.com',NULL,'Scalpellino'),
+     (2, 'Carlo','Marchiori',NULL,NULL,'carlo.marchiori@gmail.com',NULL,NULL),
+      (4, 'Carlo','Marchiori',NULL,NULL,'carlo.marchiori@gmail.com',NULL,NULL),
+       (6, 'Carlo','Marchiori',NULL,NULL,'carlo.marchiori@gmail.com',NULL,NULL),
+       (8, 'Carlo','Marchiori',NULL,NULL,'carlo.marchiori@gmail.com',NULL,NULL);
