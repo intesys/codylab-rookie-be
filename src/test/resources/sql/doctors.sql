c6ffd8390a3e904307b557d4ef0aee41b177d786
Kristian <19684@studenti.marconiverona.edu.it>
2023-06-21 18:03:56 +0200

    21/06/2023

---
+++ src/test/resources/sql/doctors.sql
@@ -0,0 +1,7 @@
+INSERT INTO doctor (id, name,surname,phone_number,address,email,avatar,profession) VALUES
+  (1,	David,	Puljic, NULL, Null, Null, Null, Null),
+  (2,	David,	Puljic, NULL, Null, Null, Null, Null),
+  (3,	David,	Puljic, NULL, Null, Null, Null, Null),
+  (4,	Kristian,	Sofric, NULL, Null, Null, Null, Null),
+  (5,	Kristian,	Sofric, NULL, Null, Null, Null, Null),
+  (6,	Kristian,	Sofric, NULL, Null, Null, Null, Null),
