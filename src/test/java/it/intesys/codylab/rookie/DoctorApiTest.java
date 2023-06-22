de87f88f0d0c34f35076c91fb92feffd7a41e679
AleBaltieri <19285@studenti.marconiverona.edu.it>
2023-06-22 17:23:22 +0200

    creazione PatientApiTest e PatientRecordApiTest

--- src/test/java/it/intesys/codylab/rookie/DoctorApiTest.java
+++ src/test/java/it/intesys/codylab/rookie/DoctorApiTest.java
@@ -1,7 +1,10 @@
 package it.intesys.codylab.rookie;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import it.intesys.codylab.rookie.api.DoctorApi;
+import it.intesys.codylab.rookie.dto.DoctorDTO;
+import it.intesys.codylab.rookie.dto.DoctorFilterDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,8 +20,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
-import it.intesys.codylab.rookie.dto.DoctorDTO;
-import it.intesys.codylab.rookie.dto.DoctorFilterDTO;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +30,9 @@ import java.util.Map;
 public class DoctorApiTest {
     private MockMvc mvc;
     @Autowired
+    ObjectMapper jsonMapper;
+    @Autowired
     WebApplicationContext context;
-    ObjectMapper jsonMapper = new ObjectMapper();
     @Autowired
     JdbcTemplate db;
 
@@ -46,7 +48,13 @@ public class DoctorApiTest {
         requestDTO.setEmail(expectedEmail);
         String jsonRequest = jsonMapper.writeValueAsString(requestDTO);
 
-        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/api/doctor").contentType(MediaType.APPLICATION_JSON).content(jsonRequest)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        MvcResult result;
+        result = mvc.perform(MockMvcRequestBuilders
+                        .post("/api/doctor")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
 
         String jsonResponse = result.getResponse().getContentAsString();
         DoctorDTO responseDTO = jsonMapper.readValue(jsonResponse, DoctorDTO.class);
@@ -65,28 +73,110 @@ public class DoctorApiTest {
     }
 
     @Test
-    @Sql(scripts = "src/test/resources/sql/doctors.sql")
+    @Sql(scripts = "/sql/doctors.sql")
     void getListDoctorTest () throws Exception {
+        String expectedSurname = "Testsurname";
+        String expectedProfession1 = "Nullafacente";
+        String expectedProfession2 = "Calciatore";
+
         DoctorFilterDTO doctorFilterDTO = new DoctorFilterDTO();
-        String expectedSurname = "Giorgio";
         doctorFilterDTO.setSurname(expectedSurname);
         String jsonRequest = jsonMapper.writeValueAsString(doctorFilterDTO);
-        String expectedProfession1 = "bellodecasa";
-        String expectedProfession2 = "pizzamandolino";
 
         MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/api/doctor/filter").contentType(MediaType.APPLICATION_JSON).param("page", "1").param("size", "2").param("sort", "profession,desc").content(jsonRequest)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
 
         String jsonResponse = result.getResponse().getContentAsString();
+
         List<DoctorDTO> doctorDTOs = jsonMapper.readValue(jsonResponse, jsonMapper.getTypeFactory().constructCollectionType(ArrayList.class, DoctorDTO.class));
         Assertions.assertNotNull(doctorDTOs);
         Assertions.assertEquals(2, doctorDTOs.size(), "doctorDTOs size");
+
         DoctorDTO doctorDTO1 = doctorDTOs.get(0);
         Assertions.assertEquals(expectedSurname, doctorDTO1.getSurname(), "doctorDTO1 surname");
         Assertions.assertEquals(expectedProfession1, doctorDTO1.getProfession(), "doctorDTO1 profession");
+
         DoctorDTO doctorDTO2 = doctorDTOs.get(1);
         Assertions.assertEquals(expectedSurname, doctorDTO2.getSurname(), "doctorDTO2 surname");
         Assertions.assertEquals(expectedProfession2, doctorDTO2.getProfession(), "doctorDTO2 profession");
     }
+
+    @Test
+    @Sql(scripts = "/sql/doctors.sql")
+    void getDoctorTest () throws Exception {
+        final Long id = 3L;
+        final String name = "Rafaello";
+        final String surname = "Testsurname";
+        final String phoneNumber = "444 7777777";
+        final String email = "rafaello.marchiori@gmail.com";
+        final String profession = "Arrotino";
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/api/doctor/{id}", id)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        String jsonResponse = result.getResponse().getContentAsString();
+        DoctorDTO doctorDTO = jsonMapper.readValue(jsonResponse, DoctorDTO.class);
+
+        Assertions.assertEquals(id, doctorDTO.getId(), "id");
+        Assertions.assertEquals(name, doctorDTO.getName(), "name");
+        Assertions.assertEquals(surname, doctorDTO.getSurname(), "surname");
+        Assertions.assertEquals(phoneNumber, doctorDTO.getPhoneNumber(), "phoneNumber");
+        Assertions.assertEquals(email, doctorDTO.getEmail(), "email");
+        Assertions.assertEquals(profession, doctorDTO.getProfession(), "profession");
+    }
+
+    @Test
+    @Sql(scripts = "/sql/doctors.sql")
+    void updateDoctorTest () throws Exception {
+        final Long id = 3L;
+        final String name = "Rafaello";
+        final String surname = "Testsurname";
+        final String phoneNumber = "444 7777777";
+        final String email = "rafaello.marchiori@gmail.com";
+
+        final String initialProfession = "Arrotino";
+        final String profession = "Calzolaio";
+
+        String currentProfession = db.queryForObject("select profession from doctor where id = ?", String.class, id);
+        Assertions.assertEquals(initialProfession, currentProfession, "initial profession");
+
+        DoctorDTO doctorDTO = new DoctorDTO();
+        doctorDTO.setName(name);
+        doctorDTO.setSurname(surname);
+        doctorDTO.setPhoneNumber(phoneNumber);
+        doctorDTO.setEmail(email);
+        doctorDTO.setProfession(profession);
+
+        String jsonRequest = jsonMapper.writeValueAsString(doctorDTO);
+
+        mvc.perform(MockMvcRequestBuilders.put("/api/doctor/{id}", id).contentType(MediaType.APPLICATION_JSON).content(jsonRequest)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        currentProfession = db.queryForObject("select profession from doctor where id = ?", String.class, id);
+        Assertions.assertEquals(profession, currentProfession, "initial profession");
+    }
+
+    @Test
+    @Sql(scripts = "/sql/doctors.sql")
+    void deleteDoctorTest () throws Exception {
+        final Long id = 3L;
+
+        Integer count = db.queryForObject("select count (*) from doctor where id = ?", Integer.class, id);
+        Assertions.assertEquals(1, count, "Doctor exists");
+
+        mvc.perform(MockMvcRequestBuilders.delete("/api/doctor/{id}", id)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+        count = db.queryForObject("select count (*) from doctor where id = ?", Integer.class, id);
+        Assertions.assertEquals(0, count, "Doctor does not exist");
+    }
+
+    @Test
+    @Sql(scripts = "/sql/doctors.sql")
+    void deleteDoctorTests() throws Exception{
+        final Long id = 3l;
+        Integer count= db.queryForObject("select count (*) from doctor where id = ?",Integer.class);
+        Assertions.assertEquals(1,count,"Doctor Exists");
+
+
+    }
+
 
     @BeforeEach
     void setup () {
--- src/test/resources/sql/doctors.sql
+++ src/test/resources/sql/doctors.sql
@@ -1,10 +1,10 @@
 INSERT INTO doctor (id, name,surname,phone_number,address,email,avatar,profession) VALUES
-  (2, 'Giovanni','Giorgio','123 4345454 ',NULL,'sdrs@gmail.com',NULL,'bellodecasa'),
-  (52, 'Giovanni','Giorgio','123 4345454',NULL,'dst@gmail.com',NULL,'pizzamandolino'),
-  (92, 'Giovanni','Giorgio','123 4345454',NULL,'df@gmail.com',NULL,'pizzamandolino'),
-  (5, 'Giovanni','Giorgio','123 4345454',NULL,'gh@gmail.com',NULL,'pizzamandolino'),
-  (6, 'Giovanni','Giorgio','343 4354656',NULL,'yu@gmail.com',NULL,'pizzamandolino'),
-  (7, 'Giovanni','Giorgio',NULL,NULL,null,NULL,NULL),
-  (1, 'Giovanni','Giorgio',NULL,NULL,null,NULL,NULL),
-  (9, 'Giovanni','Giorgio',NULL,NULL,null,NULL,NULL),
-  (34, 'Giovanni','Giorgio',NULL,NULL,null,NULL,NULL);
+  (3, 'Rafaello','Testsurname','444 7777777',NULL,'rafaello.marchiori@gmail.com',NULL,'Arrotino'),
+  (5, 'Rafaello','Testsurname','444 7777777',NULL,'rafaello.marchiori@gmail.com',NULL,'Calciatore'),
+  (9, 'Rafaello','Testsurname','444 7777777',NULL,'rafaello.marchiori@gmail.com',NULL,'Nullafacente'),
+  (7, 'Rafaello','Testsurname','444 7777777',NULL,'rafaello.marchiori@gmail.com',NULL,'Programmatore'),
+  (1, 'Rafaello','Testsurname','444 7777777',NULL,'JoeMoma@gmail.com',NULL,'Scalpellino'),
+  (2, 'Carlo','Marchiori',NULL,NULL,'carlo.marchiori@gmail.com',NULL,NULL),
+  (4, 'Carlo','Marchiori',NULL,NULL,'carlo.marchiori@gmail.com',NULL,NULL),
+  (6, 'Carlo','Marchiori',NULL,NULL,'carlo.marchiori@gmail.com',NULL,NULL),
+  (8, 'Carlo','Marchiori',NULL,NULL,'carlo.marchiori@gmail.com',NULL,NULL);
