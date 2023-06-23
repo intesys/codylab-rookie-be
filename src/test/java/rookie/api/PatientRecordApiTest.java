dda62b3e4332f6312dabeab2bcb91bf405c7f66d
3CI-SaccomandoPietro <pietro.saccomando@gmail.com>
2023-06-23 16:08:41 +0200

    test getLatestPatients, doctorIds

--- src/test/java/rookie/api/PatientRecordApiTest.java
+++ src/test/java/rookie/api/PatientRecordApiTest.java
@@ -37,21 +37,20 @@ public class PatientRecordApiTest {
     @Test
     @Sql(scripts = {"/sql/doctors.sql", "/sql/patients.sql"})
     void createPatientRecordTest () throws Exception {
-        PatientRecordDTO requestDTO = new PatientRecordDTO();
-        DoctorDTO doctorDTO = new DoctorDTO();
-        Long expectedPatientId = 32L;
-        Long expectedDoctorId = 2L;
-        String expectedTypeVisit = "Prima visita";
-        String expectedReasonVisit = "Mal di pancia";
-        String expectedTreatmentMade = "Niente";
+        Long expectedPatientId = 31L;
+        Long expectedDoctorId = 3L;
+        String expectedTypeVisit = "prima visita";
+        String expectedReasonVisit = "sto mal";
+        String expectedTreatmentMade = "niente";
 
+        PatientRecordDTO requestDTO = new PatientRecordDTO();
         requestDTO.setPatientId(expectedPatientId);
-        doctorDTO.setId(expectedDoctorId);
-        requestDTO.setDoctor(doctorDTO);
+        DoctorDTO expectedDoctorDTO = new DoctorDTO();
+        expectedDoctorDTO.setId(expectedDoctorId);
+        requestDTO.setDoctor(expectedDoctorDTO);
         requestDTO.setTypeVisit(expectedTypeVisit);
         requestDTO.setReasonVisit(expectedReasonVisit);
         requestDTO.setTreatmentMade(expectedTreatmentMade);
-
         String jsonRequest = jsonMapper.writeValueAsString(requestDTO);
 
         MvcResult result = mvc.perform(MockMvcRequestBuilders
@@ -63,34 +62,33 @@ public class PatientRecordApiTest {
 
         String jsonResponse = result.getResponse().getContentAsString();
         PatientRecordDTO responseDTO = jsonMapper.readValue(jsonResponse, PatientRecordDTO.class);
+
         Long id = responseDTO.getId();
-        Assertions.assertNotNull(id, "patient record id");
-        Assertions.assertEquals(requestDTO.getTreatmentMade(), responseDTO.getTreatmentMade(), "patient record treatment made");
+        Assertions.assertNotNull(id, "patientRecord id");
+        Assertions.assertEquals(requestDTO.getTreatmentMade(), responseDTO.getTreatmentMade(), "patientRecord treatmentMade");
 
         Map<String, Object> patientRecordMap = db.queryForMap("select * from patient_record where id = ?", id);
-        Long patient_id = ((Number) patientRecordMap.get("patient_id")).longValue();
-        Assertions.assertEquals(expectedPatientId, patient_id, "patientId");
-        Long doctor_id = ((Number) patientRecordMap.get("doctor_id")).longValue();
-        Assertions.assertEquals(expectedDoctorId, doctor_id, "doctorId");
+        Long patientId = ((Number) patientRecordMap.get("patient_id")).longValue();
+        Assertions.assertEquals(expectedPatientId, patientId, "patientId");
+        Long doctorId = ((Number) patientRecordMap.get("doctor_id")).longValue();
+        Assertions.assertEquals(expectedDoctorDTO.getId(), doctorId, "doctorId");
+        String treatmentMade = (String) patientRecordMap.get("treatment_made");
+        Assertions.assertEquals(expectedTreatmentMade, treatmentMade, "treatmentMade");
         String typeVisit = (String) patientRecordMap.get("type_visit");
         Assertions.assertEquals(expectedTypeVisit, typeVisit, "typeVisit");
-        String reasonVisit= (String) patientRecordMap.get("reason_visit");
+        String reasonVisit = (String) patientRecordMap.get("reason_visit");
         Assertions.assertEquals(expectedReasonVisit, reasonVisit, "reasonVisit");
-        String treatmentMade= (String) patientRecordMap.get("treatment_made");
-        Assertions.assertEquals(expectedTreatmentMade, treatmentMade, "treatmentMade");
-
     }
-
 
     @Test
     @Sql(scripts = {"/sql/doctors.sql", "/sql/patients.sql", "/sql/patient_records.sql"})
-    void getPatientRecordTest() throws Exception{
+    void getPatientRecordTest () throws Exception {
         Long id = 1L;
-        Long expectedPatientId = 32L;
-        Long expectedDoctorId = 2L;
-        String expectedTypeVisit = "Prima visita";
-        String expectedReasonVisit = "Mal di pancia";
-        String expectedTreatmentMade = "Niente";
+        Long expectedPatientid = 31L;
+        Long expectedDoctorId = 3L;
+        String expectedTypeVisit = "prima visita";
+        String expectedReasonVisit = "sto mal";
+        String expectedTreatmentMade = "niente";
 
         MvcResult result = mvc.perform(MockMvcRequestBuilders
                         .get(PatientRecordApi.API_PATIENT_RECORD_ID, id))
@@ -100,37 +98,37 @@ public class PatientRecordApiTest {
         String jsonResponse = result.getResponse().getContentAsString();
         PatientRecordDTO patientRecordDTO = jsonMapper.readValue(jsonResponse, PatientRecordDTO.class);
         Assertions.assertEquals(id, patientRecordDTO.getId(), "id");
-        Assertions.assertEquals(expectedPatientId, patientRecordDTO.getPatientId(), "patient id");
-        Assertions.assertEquals(expectedDoctorId, patientRecordDTO.getDoctor().getId(), "doctor id");
-        Assertions.assertEquals(expectedTypeVisit, patientRecordDTO.getTypeVisit(), "type visit");
-        Assertions.assertEquals(expectedReasonVisit, patientRecordDTO.getReasonVisit(), "reason visit");
-        Assertions.assertEquals(expectedTreatmentMade, patientRecordDTO.getTreatmentMade(), "treatment made");
+        Assertions.assertEquals(expectedPatientid, patientRecordDTO.getPatientId(), "patientId");
+        Assertions.assertEquals(expectedDoctorId, patientRecordDTO.getDoctor().getId(), "doctorId");
+        Assertions.assertEquals(expectedTypeVisit, patientRecordDTO.getTypeVisit(), "typeVisit");
+        Assertions.assertEquals(expectedReasonVisit, patientRecordDTO.getReasonVisit(), "reasonVisit");
+        Assertions.assertEquals(expectedTreatmentMade, patientRecordDTO.getTreatmentMade(), "treamentMade");
     }
 
     @Test
     @Sql(scripts = {"/sql/doctors.sql", "/sql/patients.sql", "/sql/patient_records.sql"})
-    void updatePatientRecordTest() throws Exception{
+    void updatePatientRecordTest () throws Exception {
         Long id = 1L;
-        Long expectedPatientId = 32L;
-        Long expectedDoctorId = 2L;
-        String expectedTypeVisit = "Prima visita";
-        String expectedReasonVisit = "Mal di pancia";
-        String expectedTreatmentMade = "Niente";
-        String newTreatmentMade = "Visita";
+        Long expectedPatientId = 31L;
+        Long expectedDoctorId = 3L;
+        String expectedTypeVisit = "prima visita";
+        String expectedReasonVisit = "sto mal";
+        String expectedTreatmentMade = "niente";
+        String newTreatmentMade = "qualcosa";
 
         String currentTreatmentMade = db.queryForObject("select treatment_made from patient_record where id = ?", String.class, id);
-        Assertions.assertEquals(currentTreatmentMade, expectedTreatmentMade, "initial treatmentMade");
+        Assertions.assertEquals(currentTreatmentMade, expectedTreatmentMade, "initial treament_made");
 
-        PatientRecordDTO patientRecordDTO = new PatientRecordDTO();
-        patientRecordDTO.setPatientId(expectedPatientId);
-        DoctorDTO doctorDTO = new DoctorDTO();
-        doctorDTO.setId(expectedDoctorId);
-        patientRecordDTO.setDoctor((doctorDTO));
-        patientRecordDTO.setTypeVisit(expectedTypeVisit);
-        patientRecordDTO.setReasonVisit(expectedReasonVisit);
-        patientRecordDTO.setTreatmentMade(newTreatmentMade);
+        PatientRecordDTO requestDTO = new PatientRecordDTO();
+        requestDTO.setPatientId(expectedPatientId);
+        DoctorDTO expectedDoctorDTO = new DoctorDTO();
+        expectedDoctorDTO.setId(expectedDoctorId);
+        requestDTO.setDoctor(expectedDoctorDTO);
+        requestDTO.setTypeVisit(expectedTypeVisit);
+        requestDTO.setReasonVisit(expectedReasonVisit);
+        requestDTO.setTreatmentMade(newTreatmentMade);
 
-        String jsonRequest = jsonMapper.writeValueAsString(patientRecordDTO);
+        String jsonRequest = jsonMapper.writeValueAsString(requestDTO);
 
         mvc.perform(MockMvcRequestBuilders
                         .put(PatientRecordApi.API_PATIENT_RECORD_ID, id)
@@ -140,29 +138,25 @@ public class PatientRecordApiTest {
                 .andReturn();
 
         currentTreatmentMade = db.queryForObject("select treatment_made from patient_record where id = ?", String.class, id);
-        Assertions.assertEquals(currentTreatmentMade, newTreatmentMade, "new treatmentMade");
+        Assertions.assertEquals(currentTreatmentMade, newTreatmentMade, "new treament_made");
     }
-
-
 
     @Test
     @Sql(scripts = {"/sql/doctors.sql", "/sql/patients.sql", "/sql/patient_records.sql"})
-    void deletePatientRecordTest() throws Exception {
+    void deletePatientRecordTest () throws Exception {
         final Long id = 1L;
 
-        int count = db.queryForObject("select count (*) from patient_record where id = ?", Integer.class, id);
-        Assertions.assertEquals(1, count, "patient_record exists");
-
+        Integer count = db.queryForObject("select count (*) from patient_record where id = ?", Integer.class, id);
+        Assertions.assertEquals(1, count, "PatientRecord exists");
 
         mvc.perform(MockMvcRequestBuilders
                         .delete(PatientRecordApi.API_PATIENT_RECORD_ID, id))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
+
         count = db.queryForObject("select count (*) from patient_record where id = ?", Integer.class, id);
-        Assertions.assertEquals(0, count, "patient_record does not exists");
+        Assertions.assertEquals(0, count, "PatientRecord does not exist");
     }
-
-
 
     @BeforeEach
     void setup () {
