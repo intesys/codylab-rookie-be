f420d1e97788aee5a79cf0718f1615a2f403b437
Francesco <francymannarino05@gmail.com>
2023-06-26 10:57:45 +0200

    finito

--- src/test/resources/sql/patient.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO patient (id, opd, idp, name, surname, phone_number, address, email, avatar, blood_group,avatar,profession) VALUES
-  (5, NULL, NULL, 'Qui', 'Pavarana', NULL, NULL, 'francopavarana@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
-  (6, NULL, NULL, 'Qui', 'Pavarana', NULL, NULL, 'francopavarana@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
-  (8, NULL, NULL, 'Qui', 'Pavarana', NULL, NULL, 'francopavarana@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
-  (31, NULL, NULL, 'Qui', 'Pavarana', NULL, NULL, 'francopavarana@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
-  (32, NULL, NULL, 'Qui', 'Pavarana', NULL, NULL, 'francopavarana@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
-  (33, NULL, NULL, 'Qui', 'Pavarana', NULL, NULL, 'francopavarana@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
-  (34, NULL, NULL, 'Qui', 'Pavarana', NULL, NULL, 'francopavarana@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL),
